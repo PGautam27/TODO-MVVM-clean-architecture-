c971b2ba1840e9f890f4ab9010ba180082af4630
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-01 13:07:32 +0530

    TODO-cleanArchitecture

---
+++ app/src/main/java/com/example/todokt/feature_note/data/data_Source/NoteDataBase.kt
@@ -0,0 +1,13 @@
+package com.example.todokt.feature_note.data.data_Source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.todokt.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDataBase:RoomDatabase() {
+    abstract val noteDao:NoteDao
+}
